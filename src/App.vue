58935e2f959b5b349edd8dcd2885f49984f3e2f8
sukeo-sukeo <syuke4522@gmail.com>
2020-12-12 15:50:42 +0900

    headerbar scrollでhiden add

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <side-menu></side-menu>
     </v-navigation-drawer>
 
-    <v-app-bar app flat dark>
+    <v-app-bar app flat dark hide-on-scroll>
       <v-row align="center" justify="space-between">
         <v-col cols="2" class="d-sm-none">
           <v-btn @click="drawer = !drawer"
