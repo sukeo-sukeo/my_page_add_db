30883cbdc0bc96418a974fca1973ff36e99f4880
sukeo-sukeo <syuke4522@gmail.com>
2020-12-31 06:44:18 +0900

    6th commit

--- server.js
+++ server.js
@@ -2,5 +2,4 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static(__dirname + "/docs/"));
-app.use(express.static(__dirname + "/docs/my_page/"));
 app.listen(port);
